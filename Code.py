60b5890f4e1421e7648f3e3476acfa98806da168
Aqkotz <andy.lyme@mac.com>
2018-12-04 10:30:17 -0500

    Updated from Brython Server: 12/4/2018 10:30:01 AM

--- Code.py
+++ Code.py
@@ -1,60 +1,25 @@
-import math
-from math import sin, cos, tan
-from math import pi
-from math import e
-from math import log
-from math import *
-
-A = input('Enter Function f(x): ')
-B = int(input('Enter Smallest x in Interval: '))
-C = int(input('Enter Largest x in Interval: '))
-xs = float(input("Enter a first guess for a 0: "))
-
-h = .0001
-
-D1 = []          #list of first derivatives
-D2 = []          #list of second derivatives
-XX = []          #x values
-YY = []          #Y values
-
-for a in range(B, C):
-    for b in range(10):
-        XX.append(float(a) + (float(b) / 10))
-for x in list(XX):
-    YY.append(eval(A))
-for x1 in list(XX):
-    x = x1
-    m = eval(A)
-    x = x1-h
-    f = eval(A)
-    x = x1+h
-    g = eval(A)
-    val = ((g-f)/(2*h))
-    D1.append(val)
-    lav = ((g-2*m+f)/(h**2))
-    D2.append(lav)
-
-print("For each 0.1 increase in x: ")
-print("f'(x) = ")
-print(D1)
-print("f''(x) = ")
-print(D2)
-print( )
-print("Taking your guess, we will estimate the zero using Newton's Method: ")
-print(xs)
-xf=10000000
-n=0
-while (xf-xs) > h or (xf-xs) < -h and n < 100:
-    xf=xs
-    x=xs
-    y=(eval(A))
-    x=xs+h
-    yup=(eval(A))
-    x=xs-h
-    yd=(eval(A))
-    deriv=(yup-yd)/(2*h)
-    xs=xs-(y/deriv)
-    print(round(xs,8))
-    n=n+1
-if n>99:
-    print("sorry, we can't find a 0")
+a = float(input('Enter left endpoint'))
+b = float(input('Enter right endpoint'))
+n = int(input('Enter number of subintervals'))
+f = input('Enter the function')
+LRAM = 0.0
+RRAM = 0.0
+MRAM = 0.0
+x = a
+h = (b - a)/n
+for i in range (n):
+    x1 = x
+    f1 = eval(f)
+    LRAM = LRAM + f1*h
+    x = x + h
+    x2 = x
+    f2 = eval(f)
+    RRAM = RRAM + f2*h
+    x = (x1 + x2)/2
+    fm = eval(f)
+    MRAM = MRAM + fm*h
+    x = x2
+    
+print('LRAM = ',LRAM)
+print('RRAM = ',RRAM)
+print('MRAM = ',MRAM)
